a9e1db1ba97520057f61ae33748f10f8ed909492
rahulgundawar98 <rahulgundawar98@gmail.com>
2023-05-09 09:07:53 +0530

    [Rahul]Generic example

---
+++ GenericDemo/GenericClass.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericDemo
+{
+    public class GenericClass<T>
+    {
+        //Varaible of type T
+        public T data;
+        //The Generic Constructor
+        public GenericClass(T data)
+        {
+            this.data = data;
+            Console.WriteLine("Data:"+this.data);
+        }
+
+        public void Demo(T data)
+        {
+            Console.WriteLine("Data:"+data);
+        }
+        public void Test<T>(T data)
+        {
+            Console.WriteLine("Data:" + data);
+        }
+    }
+}
---
+++ GenericDemo/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericDemo
+{
+    internal class Program
+    {
+        //Generic means general form
+        //it does not contain any datatype or not specific to any datatype
+        //we use placeholder <> this is angle bracket with type name 
+        // TypeName<T> T is your type(DataType)Parameter
+        //Genric allows us to create single class or method that can be used with different datatype
+        //Advantage- We can reuse our code
+        static void Main(string[] args)
+        {
+            GenericClass<int> genericClass = new GenericClass<int>(45);
+            GenericClass<double> genericClass1 = new GenericClass<double>(45.5);
+            GenericClass<string> generic = new GenericClass<string>("Raj");
+            generic.Demo("Reena");
+            genericClass1.Demo(12.5);
+            genericClass.Demo(60);
+
+            generic.Test<string>("N");
+            genericClass.Test(455.5);
+            Console.ReadLine();
+        }
+    }
+}
